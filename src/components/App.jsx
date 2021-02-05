1a576fc085b6e63966ee45f1ad571e2a827686ad
jawoodberry <47925152+jawoodberry@users.noreply.github.com>
2021-02-04 19:23:06 -0600

    First one Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import React, { Component, useState } from "react";
 import "../css/App.css";
+import Question from "./Question/Question";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  return (
+    <div className="app">
+      Trivia!
+      <Question question="What color is the sky?" />
+      <Question question="Uhhhh?" />
+    </div>
+  );
 }
 
 export default App;
